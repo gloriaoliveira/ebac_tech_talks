c6f90f715734de18b137be988f5e01dde1ddf56f
Gloria Oliveira <gloriost@gmail.com>
2025-04-17 19:33:41 -0300

    coomit update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2022 18:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    
+}. 1000)
